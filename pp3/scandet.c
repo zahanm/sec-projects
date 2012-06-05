bcf9ad70cabd6e180831f5c5312e02e86e1af7d4
Megan Kanne <mkanne@stanford.edu>
2012-06-05 16:04:22 -0700

    Setup to start counting SYN ACK complete. Still won't make. Need data structure.

--- pp3/scandet.c
+++ pp3/scandet.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+
+void ReadPacketData(pcap_t *file){
+  const u_char *packet;
+  struct pcap_pkthdr h;
+  packet = pcap_next(file, &h);
+  while(packet != NULL){
+    struct ip *ip_hdr = (struct ip*) (packet+14); // 14 is the offset for the Ethernet header
+    struct tcphdr *tcp_hdr = (struct tcphdr*) (packet + 14 + ip_hdr->ip_hl * 4);
+  }
+}
 
 pcap_t *OpenFile(const char *fname){
   char errbuf[PCAP_ERRBUF_SIZE];
@@ -18,7 +30,8 @@ int main(int argc, const char *argv[]){
     printf("Please provide a trace file to analyze./n");
     exit(0);
   }
- pcap_t *trace;
- trace = OpenFile(argv[1]);
-  
+  pcap_t *trace;
+  trace = OpenFile(argv[1]);
+  ReadPacketData(trace);
+  exit(0);
 }
